d1e17b22bd7b5c645b3542df4ed45357a2aca82a
tanuvit16 <tanuvit16@gmail.com>
2026-05-19 10:53:39 +0530

    feat: production ready - env config, CORS, Vercel setup

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,12 +1,70 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:8000",
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default API;
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default API;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:8000",
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
   return config;
 });
 
